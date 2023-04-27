105d4f2c993b7165abfa16625b1f0c69dc691cfe
amengistu-98 <abraham.a.mengistu@gmail.com>
2023-04-27 10:44:41 -0400

    Update practice files with my code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,17 @@
-const sumAll = function(num1, num2) {
-    if (
-        Number.isInteger(num1) === false || Number.isInteger(num2) === false ||
-        num1 < 0 || num2 < 0
-        ) {
-        return 'ERROR';
-    } else {
-        let sum = 0;
-        let lowestNum = 0;
-        let highestNum = 0;
-        if (num2 > num1) {
-            lowestNum = num1;
-            highestNum = num2;
-        } else {
-            lowestNum = num2;
-            highestNum = num1;
-        }
-        for (let i = lowestNum; i <= highestNum; i++) {
-            sum += i;
-        }
-        return sum;
+const sumAll = function(min, max) {
+    if (Number.isInteger(min) === false || Number.isInteger(max) === false) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    // reassign arguments if one is greater than the otehr
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
     }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,28 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Leap years are years divisible by four (like 1984 and 2004). 
+    // However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+    // unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). 
+    // (Yes, it's all pretty confusing)
 
+
+    // LOGIC
+    // if it IS divisible by 4 --> not a leap year
+    // if it IS divisble by 4 AND 400 --> leap year
+    // if it IS NOT divisible by 4 AND 400, but it IS divisble by 4 AND it is NOT divisible by 100 --> leap year
+
+    // MY ORIGINAL CODE
+    // if (!(year % 4 == 0)) return false;
+    // if (year % 4 == 0 && year % 400 == 0) { // checks leap years that can be divided by 400
+    //     return true;
+    // } else if (year % 4 == 0 && year % 100 != 0) { // checks for leap years that are not divisble by 100
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+
+    
 };
 
 // Do not edit below this line
