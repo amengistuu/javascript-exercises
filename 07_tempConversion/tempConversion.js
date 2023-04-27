c788b04f081bd03e288d1bad95f9e33f02fbc118
amengistu-98 <abraham.a.mengistu@gmail.com>
2023-04-27 10:58:46 -0400

    Update practice problem for tempConversion with my code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+  return round(((5/9) * (fahrenheit - 32)), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+  return round((((9/5) * celsius) + 32), 1);
 };
 
 // Do not edit below this line
